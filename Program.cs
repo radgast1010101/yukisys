97b40059bb2778ea0018bff743152accaaa9e1cf
anonUB <ub17@protonmail.com>
2026-02-12 05:51:13 +0800

    add-sample-vb-lib

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using VbBusinessLogic2;
+using VbBusinessLogic2.MyVbCode;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +22,14 @@ app.MapPost("/todos", ([FromBody] TodoItem item) =>
 {
     todos.Add(item.Task);
     return Results.Created($"/todos/{todos.Count - 1}", item.Task);
+});
+
+// sample vb.net api
+// e.g. url with param, http://localhost:5175/add?a=1&b=2
+app.MapGet("/add", (int a, int b) => {
+    var calc1 = new Class1().Add(a, b);
+    var calc2 = new Calculator().Add(a, b);
+    return calc1 + calc2;
 });
 
 app.Run();
--- obj/Debug/net9.0/MyMinApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyMinApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMinApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87666d5d2fb50c19cc37a790b0107850593ea72e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fa5d08e71aa2c70f8ef7a860603da1487acaa66")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMinApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMinApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
